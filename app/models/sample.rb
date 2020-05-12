cb42f60de9d7fea113d6c7dbd974c821b10278ef
Sunny Asar <ring2rings93@gmail.com>
2020-05-12 15:21:46 +0100

    add billing migration

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -6,13 +6,15 @@ class Sample < ApplicationRecord
   belongs_to :patient, counter_cache: true
   belongs_to :user
   belongs_to :test
+  belongs_to :bill
 
   enum status: %i[taken processing ready collected]
 
-  def add_sample(patient_id)
+  def add_sample(patient_id, bill_id)
     @sample = self
     @sample.amount = sample_amount(self[:test_id])
     @sample.patient_id = patient_id
+    @sample.bill_id = bill_id
     @sample.sample_id = SecureRandom.alphanumeric(8)
     @sample.status = 0
     @sample
---
+++ db/migrate/20200512110741_create_bills.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CreateBills < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bills, id: :uuid do |t|
+      t.string :name, null: false
+      t.references :user, type: :uuid, null: false, foreign_key: true
+      t.references :patient, type: :uuid, null: false, foreign_key: true
+      t.integer :bill_total, default: 0
+      t.decimal :paid_amount, default: 0
+      t.boolean :payment_status, default: false
+
+      t.timestamps
+    end
+  end
+end
